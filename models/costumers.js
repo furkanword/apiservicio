13185a0c9e69c9da199c249da965c14a98bca436
Auval092805 <auval0928@outlook.com>
2023-05-04 08:27:46 -0500

    Revert "Revert "🐛 empezando codigo""
    This reverts commit b4f4279eece8e2d8c418fb4e07d4e1b2d73b638c.

---
+++ models/costumers.js
@@ -0,0 +1,13 @@
+const Customers = {
+    createAt: '',
+    nombre: '',
+    apellidos: '',
+    email: '',
+    nroMovil: '',
+    fechaNacimiento: ''
+}
+export default Customers
+
+function saveData (){
+    const formData= document.querySelector('#');
+}
---
+++ server/service-costumers.js
@@ -0,0 +1,10 @@
+import costumers from "models/costumers.js";
+
+const URL_API = "https://645245cca2860c9ed406da6f.mockapi.io/";
+
+const getCostumers = async() => {
+    try {
+      const respuesta = await
+    }
+}
+
